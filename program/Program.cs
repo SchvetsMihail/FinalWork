0bac0af452b5d9791a3b6cc2d436c07aa5a1c1de
SchvetsMihail <schvets.mihail@gmail.com>
2024-04-01 20:51:22 +0300

    Корректировка описания и кода

--- program/Program.cs
+++ program/Program.cs
@@ -3,16 +3,72 @@
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-string[] Array(){
-    Console.Write("Введите количество строк, которые хотите ввести: ");
-    int n = Convert.ToInt32(Console.ReadLine());
-    string[] stringarray = new string[n];
-    for (int i = 0; i < n; i++){
-        Console.Write("Введите строку №{0}:\r\n    ", i + 1);
-        stringarray[i] = Console.ReadLine();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] workArray = FillArray();
+        string[] resultArray = GenerateNewArray(workArray);
+        string firstArray = PrintArray(workArray);
+        string secondArray = PrintArray(resultArray);
+        Console.WriteLine(firstArray + " -> " + secondArray);
+
+        string[] FillArray()
+        {
+            Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
+            string? enterSymbols = Console.ReadLine();
+            if (enterSymbols == null)
+            {
+                enterSymbols = " ";
+            };
+            char[] separators = new char[] { ',', ' ' };
+            string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            return workArray;
         }
-    for (int i = 0; i < n; i++){
-        Console.WriteLine(stringarray[i]);
+
+        string PrintArray(string[] workArray)
+        {
+            string stringArray = "[";
+            for (int i = 0; i < workArray.Length; i++)
+            {
+                if (i == workArray.Length - 1)
+                {
+                    stringArray += $"\"{workArray[i]}\"";
+                    break;
+                }
+                stringArray += $"\"{workArray[i]}\", ";
+            }
+            stringArray += "]";
+            return stringArray;
+        }
+
+        int CountStringSymbols(string[] workArray)
+        {
+            int counter = 0;
+            foreach (string item in workArray)
+            {
+                if (item.Length <= 3)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        string[] GenerateNewArray(string[] workArray)
+        {
+            int resultArrayLength = CountStringSymbols(workArray);
+            string[] resultArray = new string[resultArrayLength];
+            int i = 0;
+            foreach (string item in workArray)
+            {
+                if (item.Length <= 3)
+                {
+                    resultArray[i] = item;
+                    i++;
+                }
+            }
+            return resultArray;
+        }
     }
-    return stringarray;
 }
